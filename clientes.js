806fd5d5e408dd53f940ffc9d84bbbbe6da8ae72
srduran <srd.meg124@gmail.com>
2023-01-22 18:55:08 -0300

    librerias

--- clientes.js
+++ clientes.js
@@ -1,7 +1,16 @@
-fetch("./listaClientes.json")
-   .then(response => response.json())
+let clientesAnteriores =  [];
+
+function fetchClientes() {
+   fetch("./clientes.json")
+   .then(response => response.json ())
    .then(data => {
-    
-   })
-       
+      data.forEach(nombreCliente => {
+         nombreCliente.innerText = cliente.nombre;
+         document.body.appendChild(clientName);
+         
+      });
+   });
+   
+}
+
    
--- main.js
+++ main.js
@@ -1,18 +1,19 @@
 /**Constructor para carga de cliente**/
 
 class Cliente {
-    constructor(nombre, apellido, telefono, email, servicios) {
+    constructor(nombre, apellido, telefono, email, servicios, observaciones) {
         this.nombre = nombre;
         this.apellido = apellido;
         this.telefono = telefono;
         this.email = email;
         this.servicios = servicios;
+        this.observaciones = observaciones;
     }
 }
 
 const clientes = [];
 
-if (localStorage.getItem(clientes)) {
+if (localStorage.getItem("clientes")) {
     let cliente =JSON.parse(localStorage.getItem("clientes"));
     for (let i = 0; i < cliente.lenght; i++) {
         clientes.push(cliente[i]);
@@ -21,16 +22,19 @@ if (localStorage.getItem(clientes)) {
 
 const formulario = document.getElementById("formulario");
 
-formulario.addEventListener("submit", (e) => {
+formulario.addEventListener("submit", (e) => {    
     e.preventDefault();
     cargaCliente();
+    Swal.fire(
+        'Datos cargados correctamente',
+        'datos almacenados',
+        'success'
+      );
+    
+    
 });
 
 /**Funciones**/
-
-
-
-
 
 
 function cargaCliente () {
@@ -38,10 +42,11 @@ function cargaCliente () {
     const apellido = document.getElementById("apellido").value;
     const telefono = document.getElementById("telefono").value;
     const email = document.getElementById("email").value;
-    const servicios = document.getElementById("servicios").value;    
-    const nuevoCliente = new Cliente(nombre, apellido, telefono, email, servicios);
+    const servicios = document.getElementById("servicios").value;
+    const observaciones = document.getElementById("observaciones").value;     
+    const nuevoCliente = new Cliente(nombre, apellido, telefono, email, servicios, observaciones);
     clientes.push(nuevoCliente);
-    //Se crgan los datos del cliente en locaStorage//
+    //Se cargan los datos del cliente en locaStorage//
     localStorage.setItem("clientes", JSON.stringify(clientes));
     formulario.reset();     
 
@@ -55,24 +60,25 @@ botonCargados.addEventListener("click", () => {
     informacionCliente ();
 });
 
-function selectServicios(){
-    const servicios = document.getElementById("servicios").checked;
-}
+
 
 function informacionCliente() {
     datosCargadosClientes.innerHTML = "";
     clientes.forEach((cliente) => {
-        const div=document.createElement("div");
+
+        const div = document.createElement("div");
         div.innerHTML = `
                           <div>
                               <p> Nombre Cliente : ${cliente.nombre}</p>
                               <p> Apellido Cliente : ${cliente.apellido}</p>                              
                               <p> Telefono Cliente : ${cliente.telefono}</p>
                               <p> Email Cliente : ${cliente.email}</p>
-                              <p> Servicio Realizado: ${cliente.servicios}</p>                            
+                              <p> Servicio Realizado: ${cliente.servicios}</p>
+                              <p> Observaciones: ${cliente.observaciones}</p>                            
                           </div>
                         `;
       datosCargadosClientes.appendChild(div);                  
 
     });
 }
+
